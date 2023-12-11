a88bf7cb9820b97b1fc48424c80ecdee8123da89
Octawel <serbanrares455@gmail.com>
2023-12-12 00:15:16 +0200

    initializari

--- Proiect/proiect.py
+++ Proiect/proiect.py
@@ -2,3 +2,9 @@ import copy
 import random
 import tkinter as tk
 
+class XSiO:
+    def __init__(self, dificultate=1):
+        self.tabla = [[' ' for _ in range(3)] for _ in range(3)]
+        self.jucator_curent = 'X'
+        self.castigator = None
+        self.dificultate = dificultate
